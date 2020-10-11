2e11de84c0fd6cf590a048dbc3d555654eac7e3e
Akshai <akshaikp@karunya.edu.in>
2020-10-11 17:44:25 +0530

    final

--- app.py
+++ app.py
@@ -11,16 +11,16 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
+    int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='The bank note is $ {}'.format(output))
+    output = prediction[0]
+    if(output == 0):
+        pred = 'not authentic'
+    else:
+        pred = 'authentic'
+    return render_template('index.html', prediction_text='The bank note is {}'.format(pred))
 
 if __name__ == "__main__":
     app.run(debug=True)
